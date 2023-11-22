a69a14de709e8b5e6924708c16c07bab27cdc7b5
= <=>
2023-11-22 12:23:14 -0300

    configuracao do ambiente de teste

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into usuarios(id,username,password,role) values(100,'ana@gmail.com','321654','ROLE_ADMIN');
+insert into usuarios(id,username,password,role) values(101,'paula@gmail.com','321654','ROLE_CLIENTE');
+insert into usuarios(id,username,password,role) values(102,'vania@gmail.com','321654','ROLE_CLIENTE');
