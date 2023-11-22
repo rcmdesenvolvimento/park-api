3266709a5c43e556b422dc3fa914fa181e10a89e
= <=>
2023-11-22 17:31:37 -0300

    teste ponta a ponta para criar um novo usuario

--- src/test/java/com/rcm/sistemas/parkapi/UsuarioITTest.java
+++ src/test/java/com/rcm/sistemas/parkapi/UsuarioITTest.java
@@ -1,7 +1,12 @@
 package com.rcm.sistemas.parkapi;
 
+import com.rcm.sistemas.parkapi.web.dto.UsuarioCreateDto;
+import com.rcm.sistemas.parkapi.web.dto.UsuarioResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -13,5 +18,20 @@ public class UsuarioITTest {
     @Autowired
     WebTestClient webTestClient;
 
-
+    @Test
+    public void createUsuario_ComUserNameEPasswordValidos_RetornaUsuarioCriadoComStatus201() {
+        UsuarioResponseDto usuarioResponseDto = webTestClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("vasco@gmail.com", "vascao"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+        Assertions.assertThat(usuarioResponseDto).isNotNull();
+        Assertions.assertThat(usuarioResponseDto.getId()).isNotNull();
+        Assertions.assertThat(usuarioResponseDto.getUserName()).isEqualTo("vasco@gmail.com");
+        Assertions.assertThat(usuarioResponseDto.getRole()).isEqualTo("CLIENTE");
+    }
 }
