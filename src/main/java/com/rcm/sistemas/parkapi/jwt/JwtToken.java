4744f816ba054330a83642a38c05e55db647459c
= <=>
2023-12-09 16:27:03 -0300

    criação do objeto de resposta JwtToken

---
+++ src/main/java/com/rcm/sistemas/parkapi/jwt/JwtToken.java
@@ -0,0 +1,14 @@
+package com.rcm.sistemas.parkapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+    private String token;
+}
